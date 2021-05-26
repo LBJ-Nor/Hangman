13e9d76a11334e4c5eae7fc2c33ab12a6f8a7406
LBJ-Nor <80926104+LBJ-Nor@users.noreply.github.com>
2021-05-26 05:26:24 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,53 @@
+from hangman_art import models
+import random
+
+with open('wordlist.txt') as f:
+    secret_word = (random.choice(f.readlines()))
+    secret_word = secret_word.upper()
+    secret_word = secret_word[:-1]
+
+already_guessed = []
+wrong_guess_counter = 0
+placeholder = ['_' for _ in range(len(secret_word))]
+
+
+def guess_validator():
+    global wrong_guess_counter
+    player_guess = input('Guess a letter: ')
+    player_guess = player_guess.upper()
+
+    if player_guess.isalpha():
+        if len(player_guess) == 1:
+            if player_guess in already_guessed:
+                print('That letter has already been guessed. Try again.')
+            else:
+                already_guessed.append(player_guess)
+                if player_guess not in secret_word:
+                    wrong_guess_counter += 1
+
+            for s in range(len(secret_word)):
+                if secret_word[s] == player_guess:
+                    placeholder[s] = player_guess
+
+        else:
+            print('Guess one letter at a time. Try again.')
+
+    else:
+        print('That was not a letter. Try again.')
+
+
+# Game loop
+while '_' in placeholder:
+    print(f"""{models[wrong_guess_counter]}
+Guess the word:  {' '.join(placeholder)}
+Already guessed: {', '.join(already_guessed)}
+""")
+    guess_validator()
+    if wrong_guess_counter == 6:
+        print(f'The word was {secret_word}')
+        print('You died!')
+        break
+
+if '_' not in placeholder:
+    print(f' The word was {secret_word}')
+    print('You win!')
